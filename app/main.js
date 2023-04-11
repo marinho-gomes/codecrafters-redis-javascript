c6d07adea62b276eb0da173705bce1aaff4e574f
Marinho Gomes <32732849+marinho-gomes@users.noreply.github.com>
2023-04-11 09:21:18 -0300

    ping response

--- app/main.js
+++ app/main.js
@@ -6,6 +6,11 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   // Handle connection
+  connection.on("data", () => {
+    console.log('PONG');
+    connection.write('+PONG\r\n');
+  })
 });
+
 //
 server.listen(6379, "127.0.0.1");
